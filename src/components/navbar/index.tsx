fb115785102bfafac3d46da835a5371b811d93ef
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-10-02 17:47:24 -0300

    feat: navegação das categorias com swiper
    adicionado a biblioteca swiper para melhorar a navegação das categorias em telas menores

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { useContext, useState } from "react";
+import 'swiper/css';
+import 'swiper/css/free-mode'
 import styles from "./navbar.module.css";
 import toast from "react-hot-toast";
 import { IoAddCircle } from "react-icons/io5";
 
 import { Modal } from "@/components/Modal";
 import { AuthContext } from "@/context/AuthContext";
-import Link from "next/link";
 import { CategoriesProps } from "@/utils/types/Product";
 import { MdDelete } from "react-icons/md";
 import { useRouter } from "next/navigation";
 import { deleteDoc, doc } from "firebase/firestore";
 import { db } from "@/services/firebaseConnection";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from 'swiper/modules'
 
 interface NavbarProps {
     data: CategoriesProps[]
@@ -33,14 +37,71 @@ export function Navbar({ data }: NavbarProps) {
 
         } catch (error) {
             console.log(error)
-            throw new Error("Erro ao atualizar a categoria")
+            throw new Error("Erro ao excluir categoria")
         }
     }
 
     return (
         <>
             <nav className={styles.navbar}>
-                {data.map((item) => (
+                <Swiper
+                    className={styles.swiper}
+                    modules={[FreeMode]}
+                    slidesPerView={2}
+                    spaceBetween={10}
+                    grabCursor={true}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 4,
+                            spaceBetween: 20,
+                          },
+                          768: {
+                            slidesPerView: 5,
+                            spaceBetween: 40,
+                          },
+                          1024: {
+                            slidesPerView: 6,
+                            spaceBetween: 50,
+                          },
+                    }}
+                >
+                    {data && data.map((category) => (
+                        <SwiperSlide 
+                            onClick={() => {
+                                if (signed) {
+                                    router.push(`/categories/${category.id}`);
+                                } else {
+                                    window.location.hash = category.name;
+                                }
+                            }}
+                            key={category.id} 
+                            className={styles.swiper_slide}
+                        >
+                            {category.name}
+                            {signed && (
+                            <button
+                                className={styles.buttonDelete}
+                                onClick={() => {
+                                    setShowModal(true)
+                                    setIdDelete(category.id)
+                                }}
+                            >
+                                <MdDelete size={20} color='#20170E' />
+                            </button>
+                        )}
+                        </SwiperSlide>
+                    ))}
+                    {signed && (
+                        <SwiperSlide 
+                            className={styles.swiper_add}
+                            onClick={() => router.push('/categories/new')}
+                        >
+                            <IoAddCircle size={28} color="#FFB700" />
+                        </SwiperSlide>
+                    )}
+                </Swiper>
+
+                {/* {data.map((item) => (
                     <li className={styles.categorieItem} key={item.id}>
                         <button
                             className={styles.button}
@@ -71,7 +132,9 @@ export function Navbar({ data }: NavbarProps) {
                     <Link href={"/categories/new"} className={styles.buttonAdd}>
                         <IoAddCircle size={28} color="#FFB700" />
                     </Link>
-                )}
+                )} */}
+
+
             </nav>
             <Modal
                 isOpen={showModal}
