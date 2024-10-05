30d2a4c954e720806c8fd26a77257531367a80ae
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-10-05 17:09:44 -0300

    test: ajuste em funções do randle router

--- src/utils/functions/firebaseFunctions.ts
+++ src/utils/functions/firebaseFunctions.ts
@@ -2,8 +2,6 @@ import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { CategoriesProps, ProductProps } from "../types/Product";
 import { db } from "@/services/firebaseConnection";
 
-
-
 export async function fetchProducts(): Promise<ProductProps[]> {
     try {
         const productsRef = collection(db, 'products');
@@ -30,18 +28,23 @@ export async function fetchProducts(): Promise<ProductProps[]> {
 }
 
 export async function fetchCategories(): Promise<CategoriesProps[]> {
-    const categoriesRef = collection(db, 'categories');
-    const q = query(categoriesRef, orderBy('name', 'asc'))
-    const snapshot = await getDocs(q)
-    let listCategories: CategoriesProps[] = [];
+    try {
+        const categoriesRef = collection(db, 'categories');
+        const q = query(categoriesRef, orderBy('name', 'asc'))
+        const snapshot = await getDocs(q)
+        let listCategories: CategoriesProps[] = [];
 
-    snapshot.forEach(doc => {
-        listCategories.push({
-            id: doc.id,
-            name: doc.data().name,
-            created: doc.data().created,
+        snapshot.forEach(doc => {
+            listCategories.push({
+                id: doc.id,
+                name: doc.data().name,
+                created: doc.data().created,
+            })
         })
-    })
 
-    return listCategories
+        return listCategories
+    } catch (error) {
+        console.log('Erro ao buscar categorias', error)
+        return []
+    }
 }
