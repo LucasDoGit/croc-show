8a19e1d19b4f67afd8a97b964dcce3ab63e96c4d
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-10-05 16:35:45 -0300

    test: ajustando rota das categorias

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,26 +1,16 @@
 import { db } from '@/services/firebaseConnection';
+import { fetchCategories } from '@/utils/functions/firebaseFunctions';
 import { CategoriesProps } from '@/utils/types/Product';
 import { collection, getDocs, orderBy, query } from 'firebase/firestore';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
     try {
-        const categoriesRef = collection(db, 'categories');
-        const q = query(categoriesRef, orderBy('name', 'asc'))
-        const snapshot = await getDocs(q)
-        let listCategories: CategoriesProps[] = [];
-
-        snapshot.forEach(doc => {
-            listCategories.push({
-                id: doc.id,
-                name: doc.data().name,
-                created: doc.data().created,
-            })
-        })
-
-    return NextResponse.json(listCategories);
+        const categories: CategoriesProps[] = await fetchCategories();
+    
+        return NextResponse.json(categories);
     } catch (error) {
-        console.log("Erro ao buscar categorias", error)
-        return NextResponse.json({ error: 'Erro ao buscar categorias' }, { status: 500 })
+        console.log("Erro ao buscar categorias", error);
+        return NextResponse.json({ error: 'Erro ao buscar categorias' }, { status: 500 });
     }
 }
