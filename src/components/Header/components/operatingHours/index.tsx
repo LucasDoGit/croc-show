e2c58840e3719d3e6be62a9e5147c31ecbee58d7
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-10-30 00:56:47 -0300

    fix: horário corresponde ao dispositivo
    o horário buscado estava vindo do servidor e assim não correspondia ao dispositivo do cliente. Alterando também o funcionamento da loja para o correto.

--- src/components/Header/components/operatingHours/index.tsx
+++ src/components/Header/components/operatingHours/index.tsx
@@ -1,15 +1,25 @@
+"use client"
 import styles from './operatinghours.module.css'
 
 export function OperatingHours() {
-  const openingTime = 19;  // 9 AM
-  const closingTime = 23; // 10 PM
   const currentHour = new Date().getHours();
+  const currentDay = new Date().getDay();
+  
+  let openingTime = 20;
+  let closingTime = 1;
+  let operatingDaysText = "Seg á Sex";
 
-  const isOpen = currentHour >= openingTime && currentHour < closingTime;
+  if(currentDay === 0 || currentDay === 6){
+    openingTime = 11
+    closingTime = 22
+    operatingDaysText = "Sab e Dom";
+  }
+
+  const isOpen = (currentHour >= openingTime && currentHour < 24) || (currentHour >= 0 && currentHour < closingTime);
 
   const bgColor = isOpen ? '#54CC0A' : '#f34e26';
 
   return (
-    <span className={styles.openingHours} style={{backgroundColor: `${bgColor}`}}>Seg á Dom - 19:00 as 23:00</span>
+    <span className={styles.openingHours} style={{backgroundColor: `${bgColor}`}}>{operatingDaysText} - {openingTime}:00 as {closingTime}:00</span>
   );
 };
